448dd345ad9579e4cc2acc36743d5cbca2f1a10f
kadenhargrove <kadenhargrove@gmail.com>
2023-04-30 15:40:57 -0500

    finish
    Co-authored-by: Thomas Gray <tcg3235@gmail.com>

--- Classes.java
+++ Classes.java
@@ -1,7 +1,15 @@
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
+
+
+
 class Person implements Cloneable {
     private String name;
     private String ID;
     private String SSN;
+    private static Pattern pattern = Pattern.compile("^[0-9]{9}$");
+
 
     public Person(String name, String id, String SSN) {
         this.name = name;
@@ -22,8 +30,15 @@ class Person implements Cloneable {
         this.ID = id;
     }
 
+    // Avoiding CWE-233: Improper Handling of Parameters
     public void setSSN(String SSN) {
-        this.SSN = SSN;
+        Matcher matcher = pattern.matcher(SSN);
+        if (matcher.matches()) {
+            this.SSN = SSN;
+        }
+        else {
+            System.out.println("Invalid SSN");
+        }
     }
 
     // CWE-767: Access to Critical Private Variable via Public Method
@@ -105,14 +120,30 @@ class Employee extends Person {
 
 public class Classes {
     public static void main(String[] args) throws Exception {
+        Pattern pattern = Pattern.compile("^[0-9]{9}$"); // simple SSN validation
+
+
         Person p = new Person("Thomas Gray", "tcgray2", "888888888");
         // CWE-492: Use of Inner Class containing sensitive data
         Person.Student s = p.new Student(p.getName(), p.getID(), p.getSSN(), 3.5); // I don't want to EVER get the SSN
         System.out.println(s.toString());
-        Person newPerson = new Employee("Thomas", "8828282", "981y59018y", "89889190");
+        Person newPerson = new Employee("Thomas", "8828282", "898913909", "981y59018y");
         Person newerPerson = newPerson.clone();
         newerPerson.toString();
         System.out.println(newPerson.toString());
+
+        // CWE-625: Permissive Regular Expression - avoid allowing SSN's that are not 9 numbers
+        Matcher matcher = pattern.matcher(newPerson.getSSN());
+        if (matcher.matches()) {
+            System.out.println("The SSN has 9 numbers");
+        }
+
+        newPerson.setSSN("9108509187590187");
+
+        // Avoiding CWE-187: Partial String Comparison -- make sure you compare entire strings (Java)
+        if (newPerson.getID().equals(newerPerson.getID())) {
+            System.out.println("The two people's work IDs match");
+        }
 
         // CWE-486: Comparison of Classes by Name
         // if (p.getClass().equals("Student") - don't do this
@@ -123,6 +154,13 @@ public class Classes {
         }
 
         Person testPerson = new Employee("TestName", "222", "3333333333", "111111111").getEmployee();
+        Person testPerson2 = new Employee("TestName", "222", "3333333333", "111111111").getEmployee();
+        
+        // CWE-595: Comparison of Object References Instead of Object Contents 
+        if (testPerson.equals(testPerson2)) {
+            //should return false
+            System.out.println("The two test people are equal");
+        }
         System.out.println(testPerson.toString());
         // CWE-586: Explicit Call to Finalize()
     }
